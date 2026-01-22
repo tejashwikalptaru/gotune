a6c6e801344cce703805130dd8e9a0800fd26d0d
tejashwikalptaru <tejashwi.1991@gmail.com>
2026-01-22 15:53:34 +0530

    architecture refactoring

--- internal/adapter/audio/bass/platform_linux.go
+++ internal/adapter/audio/bass/platform_linux.go
@@ -5,7 +5,10 @@ package bass
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/../../../../build/libs/linux
-#cgo LDFLAGS: -L${SRCDIR}/../../../../build/libs/linux -lbass -Wl,-rpath,$$ORIGIN/../libs
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../../../../build/libs/linux/x86_64 -lbass -Wl,-rpath,$$ORIGIN/../libs
+#cgo linux,386 LDFLAGS: -L${SRCDIR}/../../../../build/libs/linux/x86 -lbass -Wl,-rpath,$$ORIGIN/../libs
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../../../build/libs/linux/aarch64 -lbass -Wl,-rpath,$$ORIGIN/../libs
+#cgo linux,arm LDFLAGS: -L${SRCDIR}/../../../../build/libs/linux/armhf -lbass -Wl,-rpath,$$ORIGIN/../libs
 #include "bass.h"
 */
 import "C"
